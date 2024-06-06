11f4b9f0a5aaaefa2024f4fd4e72f62352898d5f
Renan Mattos <mattosrenan5@gmail.com>
2024-06-06 10:50:13 -0300

    Create tic.js

---
+++ tic.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const typed = new Typed('.multiple-text', {
+        strings: ['Seus momentos.', 'Seu casamento.', 'Seu aniversario.', 'Sua formatura.'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+
+});
